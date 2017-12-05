0e2aaf4f3c26d65c79fa221e5ad4bae6999ae5e4
Family\User <jorge.iza@epn.edu.ec>
2017-12-05 16:19:14 -0500

    En este Archivo ya esta incluido el codigo para contar las palabras y el archivo (textoPython.txt)

--- ContarPalabras/ContadorDePalabras.py
+++ ContarPalabras/ContadorDePalabras.py
@@ -1,20 +1,37 @@
-texto = "La ciudad fin de Buenos Aires. ciudad es conocida es conocida como la paris de America"
-palabra = texto.split(" ")
-print(palabra)
-miNuevaLista =  list(set(palabra))
+#contador de palabras
 
-contPalabrasTotal = 0;
-contPalabra = 0;
-#Cuento el numero total de palabras que hay en el texto
-print("El número total de palabras son: ",len(palabra))
-print()
-#Cuento cuantas veces se esta repitiendo la mima palabra en el texto
-for i in range (0,len(miNuevaLista)):
-    palabraTemp = miNuevaLista[i]
-    for j in range (0,len(palabra)):
-        if(palabra[j] == palabraTemp):
-            contPalabra=contPalabra+1;
-    print('"'+palabraTemp+'":',contPalabra)
-    contPalabra=0;
+def leertxt():
+    print("")
+    archi=open('textoPython.txt','r')
+    lineas=archi.readline()
+    print(lineas)
+    palabra = lineas.split()
+    miNuevaLista = list(set(palabra))
+    contador=0
 
+    contPalabrasTotal = 0;
+    contPalabra = 0;
+
+    print("")
+    for p in range (0,len(palabra)):
+        contador+=1
+
+    # Cuento el numero total de palabras que hay en el texto
+    print("El número total de palabras son: ", len(palabra))
+    print()
+    # Cuento cuantas veces se esta repitiendo la mima palabra en el texto
+    for i in range(0, len(miNuevaLista)):
+        palabraTemp = miNuevaLista[i]
+        for j in range(0, len(palabra)):
+            if (palabra[j] == palabraTemp):
+                contPalabra = contPalabra + 1;
+        print('"' + palabraTemp + '":', contPalabra)
+        contPalabra = 0;
+
+    print("##################### ARREGLO ######################\n")
+    print(palabra)
+    print("El total de palabras es: "+str(contador))
+    archi.close()
+
+leertxt()
 
